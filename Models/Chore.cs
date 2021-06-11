014a32c005ab1df384d210388689a603f079b8c6
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-06-11 15:28:56 -0500

    First commit

---
+++ Models/Chore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomMates.Models
+{
+    // C# representation of the Chore table
+    public class Chore
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
